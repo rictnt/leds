3da21832caca1fe5dd247ad55e9ac434fc73b3dd
rictnt <rictnt@gmail.com>
2020-03-29 15:20:31 -0500

    añaimos el index en html y php

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>SecurityPi</title>
+<style type="text/css">
+body,td,th {
+color: #000000;
+font-family: Gotham, "Helvetica Neue", Helvetica, Arial, sans-
+serif;
+}
+body {
+background-image: url();
+background-repeat: repeat-x;
+background-color: #FFFFFF;
+}
+</style>
+<div align="center"><a href="index.html"><img src="Cabecera.jpg"
+></a></div>
+</head>
+<body>
+<hr>
+<center>
+<table width="520" border="1">
+<tbody>
+<tr>
+<td width="847" height="66" style="text-align: center; font-
+family: Gotham, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-
+style: normal;"><h2><strong>Iniciar Sesion</strong></h2></td>
+</tr>
+<tr>
+<td height="170"><form id="form1" name="form1" method="post"
+action="login.php">
+<p align="center">
+<label for="textfield"></label>
+Usuario:
+<input type="text" name="user">
+</p>
+<p align="center">ContraseÃ±a:
+<input type="password" name="pass" >
+</p>
+<div align="center">
+<input type="submit" name="submit" value="Acceder" />
+</div>
+</form></td>
+</tr>
+</tbody>
+</table>
+</center>
+</body>
+</html>
